20588e68a52670dd6b845151ace227adeee373c0
cracycot <cracycot@mail.ru>
2025-01-23 00:23:02 +0300

    feat: made ConnectionFactory

--- src/main/java/org/example/utils/RedisConfig.java
+++ src/main/java/org/example/utils/RedisConfig.java
@@ -1,18 +1,34 @@
 package org.example.utils;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+
         return template;
+    }
+
+    @Bean
+    public ZSetOperations<String, Long> zSetOperations(RedisTemplate<String, Long> redisTemplate) {
+        return redisTemplate.opsForZSet();
     }
 }
